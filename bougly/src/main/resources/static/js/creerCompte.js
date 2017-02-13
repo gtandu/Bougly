df36e1f076f43b1438931187dea66f9a57ec4240
gtandu <tbrandon91@hotmail.fr>
2017-02-13 23:56:11 +0100

    [ADMINISTRATEUR] Page Html - Creer un compte : Controle de saisie des champs

--- bougly/src/main/resources/static/js/creerCompte.js
+++ bougly/src/main/resources/static/js/creerCompte.js
@@ -1,8 +1,15 @@
 $(function() {
 	$('select').material_select();
+	$('#numeroEtudiant').characterCounter();
 	hideAttributOnRoleNoEtudiant();
+	checkDateFormatOnInputFocusOut();
+	checkErrorOnSubmit();
 });
 
+/**
+ * Cache les inputs selon le role choisit
+ * @returns
+ */
 function hideAttributOnRoleNoEtudiant() {
 	var currentRole = $('select option:selected').val()
 	console.log(currentRole);
@@ -17,4 +24,66 @@ function hideAttributOnRoleNoEtudiant() {
 			});
 		}
 	});
+}
+
+/**
+ * Verifie le format de la date en sortie d'input
+ * dd/MM/YYYY Ex : 01/01/2017
+ * @returns
+ */
+function checkDateFormatOnInputFocusOut()
+{
+	$("#dateDeNaissance").focusout(function(){
+		var inputDateDeNaissance = $(this).val();
+		var dateValide = isValidDate(inputDateDeNaissance);
+		console.log(dateValide);
+		if(dateValide == false)
+		{
+			console.log($(this));
+			$(this).addClass('invalid');
+		}
+		else
+		{
+			$(this).removeClass('invalid').addClass('valid');
+		}
+	});
+}
+
+/**
+ * Test le format de la date de l'input
+ * @param date
+ * @returns
+ */
+function isValidDate(date)
+{
+	var reg = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
+
+	return reg.test(date);
+	
+}
+
+/**
+ * Verifie lors du submit que tous les champs soient valides
+ * @returns
+ */
+function checkErrorOnSubmit()
+{
+	$("#submitButton").click(function(event){
+		event.preventDefault();
+		var error = false;
+		$('input').each(function(){
+			var currentInput = $(this);
+			
+			if(currentInput.val()=='' || currentInput.hasClass('invalid'))
+			{
+				error = true
+				currentInput.addClass('invalid');
+			}
+		})
+		if(error == false)
+		{
+			$("form").submit();
+		}
+	})
+	
 }
