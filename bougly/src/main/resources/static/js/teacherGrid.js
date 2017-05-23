3b3ef69c7df12b30bdd4fa60614cae2c93018548
cmapella <mapella.corentin@gmail.com>
2017-05-23 19:32:10 +0200

    #19

--- bougly/src/main/resources/static/js/teacherGrid.js
+++ bougly/src/main/resources/static/js/teacherGrid.js
@@ -5,14 +5,15 @@ var clients = [
         { "Nom": "BATTAIS", "Prénom": "Benjamin", "Note Continue": 6, "Note Partiel": 12,},
         { "Nom": "SEURIN", "Prénom": "Lucie", "Note Continue": 7, "Note Partiel": 16,},
         { "Nom": "VACHON", "Prénom": "Benoit", "Note Continue": 10, "Note Partiel": 2,},
-        { "Nom": "ELGOFF", "Prénom": "Saher", "Note Continue": 12, "Note Partiel": 17,},
-    ];
+        { "Nom": "ELGOFF", "Prénom": "Saher", "Note Continue": 12, "Note Partiel": 17},
   
+        ];
+
     $("#jsGrid").jsGrid({
         width: "100%",
         //height: "400px",
+        
  
-        inserting: true,
         editing: true,
         sorting: true,
         paging: true,
@@ -20,11 +21,12 @@ var clients = [
         data: clients,
  
         fields: [
-            { name: "Nom", type: "text", width: 100, validate: "required" },
-            { name: "Prénom", type: "text", width: 100 },
-            { name: "Note Continue", type: "number", width: 50 },
-            { name: "Note Partiel", type: "number", width: 50 },
-            { type: "control" }
+            { name: "Nom", width: 100, align: "center" },
+            { name: "Prénom", width: 100, align: "center" },
+            { name: "Note Continue", type: "number", width: 50, align: "center" },
+            { name: "Note Partiel", type: "number", width: 50, align: "center" },
+            { name: "Moyenne", width: 50, align: "center" },
+            { type: "control", deleteButton: false, }
         ]
     });
     
