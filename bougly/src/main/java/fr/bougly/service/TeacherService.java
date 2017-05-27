9461ba7ccc0355122847e46a27be6b597a8f1823
cmapella <mapella.corentin@gmail.com>
2017-05-27 13:33:45 +0200

    Improve Class Diagram #26

--- bougly/src/main/java/fr/bougly/service/TeacherService.java
+++ bougly/src/main/java/fr/bougly/service/TeacherService.java
@@ -1,11 +1,14 @@
 package fr.bougly.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import fr.bougly.repository.TeacherRepository;
 
 @Service
 public class TeacherService {
 
+	@Autowired
+	private TeacherRepository teacherRepository;
+	
 }
