0089f330b9e75dbc7c3f46b602b49f736421cd87
cmapella <mapella.corentin@gmail.com>
2017-06-23 01:16:57 +0200

    Fix Student error

--- diptrack/src/main/java/fr/diptrack/web/controller/StudentController.java
+++ diptrack/src/main/java/fr/diptrack/web/controller/StudentController.java
@@ -1,8 +1,14 @@
 package fr.diptrack.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import fr.diptrack.model.UserAccount;
+import fr.diptrack.service.AccountService;
 
 @Controller
 @RequestMapping(value = "/etudiant")
@@ -11,11 +17,19 @@ public class StudentController {
 	public static final String URL_STUDENT_HOME_PAGE = "/accueilEtudiant.html";
 	public static final String URL_STUDENT_MARK_PAGE = "/noteEtudiant.html";
 
+	@Autowired
+	private AccountService accountService;
+	
 	@RequestMapping(value = URL_STUDENT_HOME_PAGE)
 	public ModelAndView showStudentHomePage() {
 		ModelAndView model = new ModelAndView();
-		model.setViewName("accueilEtudiant");
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		UserAccount account = accountService.findByMail(auth.getName());
 
+		model.addObject("account", account);
+		model.setViewName("accueilEtudiant");
+				
 		return model;
 	}
 
---
+++ diptrack/src/main/resources/static/js/tooltips.js
@@ -0,0 +1,10 @@
+$(function(){
+	tooltips();
+})
+
+function tooltips()
+{
+	$(document).ready(function(){
+	    $('.tooltipped').tooltip({delay: 50});
+	  });
+}
