5a06fd93064932967a3474a4a2bfd56e6208d4fb
gtandu <tbrandon91@hotmail.fr>
2017-06-26 17:40:11 +0200

    Fix error message login in session

--- diptrack/src/main/java/fr/diptrack/web/controller/LoginController.java
+++ diptrack/src/main/java/fr/diptrack/web/controller/LoginController.java
@@ -1,9 +1,13 @@
 package fr.diptrack.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,7 +26,14 @@ public class LoginController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping(value = URL_LOGIN_PAGE, method = RequestMethod.GET)
-	public ModelAndView showLoginPage() throws Exception {
+	public ModelAndView showLoginPage(HttpServletRequest request, Model model) throws Exception {
+		
+		String errorMsg = (String) request.getSession().getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		if(errorMsg != null)
+		{
+			model.addAttribute("errorMsg", errorMsg);
+			request.getSession().removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		}
 
 		//initService.initUser();
 		//initService.initClass();
